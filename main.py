b849e0850fc94b5b6a9b3af751870fc8ef8e1ac9
Hazza <86918289+Hazza3100@users.noreply.github.com>
2022-07-08 20:15:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import discord
 import requests
 import random
+import json
+
+
 from discord.ext import commands
 
 
 
-prefix = "."
-token = ""
-color = 0xfb00ff
+with open('config.json') as f:
+    cfg = json.load(f)
+
+token = cfg['bot_token']
+prefix = cfg['prefix']
+color = cfg['color']
+use_proxy = cfg['use_proxy']
+role_1 = cfg['role1']
+role_2 = cfg['role2']
+role_3 = cfg['role3']
+role_4 = cfg['role4']
+role_5 = cfg['role5']
+
+default_amount = cfg['default_amount']
+role1_amount = cfg['role1_amount']
+role2_amount = cfg['role2_amount']
+role3_amount = cfg['role3_amount']
+role4_amount = cfg['role4_amount']
+role5_amount = cfg['role5_amount']
+
+
 
 bot = commands.Bot(command_prefix=prefix, help_command=None)
 
@@ -46,6 +67,15 @@ def follow_user(username):
     token = open('tokens.txt', 'r').read().splitlines()
     tokens = random.choice(token)
 
+    proxy_list = open('proxies.txt','r').read().splitlines()
+
+
+    proxy = random.choice(proxy_list)
+    proxies = {
+    'http': f'http://{proxy}',
+    'https':f'http://{proxy}'
+    }
+
     headers = {
         "Accept": "*/*",
         "Accept-Encoding": "gzip, deflate, br",
@@ -83,7 +113,10 @@ def follow_user(username):
         }
     }
     }
-    r = requests.post('https://gql.twitch.tv/gql', headers=headers, json=json)
+    if use_proxy == False:
+        r = requests.post('https://gql.twitch.tv/gql', headers=headers, json=json)
+    else:
+        r = requests.post('https://gql.twitch.tv/gql', headers=headers, json=json, proxies=proxies)
 
 
 
@@ -93,7 +126,24 @@ async def follow(ctx, channel_name):
 
     username = get_user(channel_name)
 
-    follow_amount = 25
+    role1 = discord.utils.get(ctx.guild.roles, name=role_1)
+    role2 = discord.utils.get(ctx.guild.roles, name=role_2)
+    role3 = discord.utils.get(ctx.guild.roles, name=role_3)
+    role4 = discord.utils.get(ctx.guild.roles, name=role_4)
+    role5 = discord.utils.get(ctx.guild.roles, name=role_5)
+
+    follow_amount = default_amount
+
+    if role1 in ctx.author.roles:
+        follow_amount = role1_amount
+    elif role2 in ctx.author.roles:
+        follow_amount = role2_amount
+    elif role3 in ctx.author.roles:
+        follow_amount = role3_amount
+    elif role4 in ctx.author.roles:
+        follow_amount = role4_amount
+    elif role5 in ctx.author.roles:
+        follow_amount = role5_amount
 
     embed = discord.Embed(title="Twitch followers", description=f"Sending **{follow_amount}** Twitch Followers to **{username}**", color=color)
     await ctx.send(embed=embed)
